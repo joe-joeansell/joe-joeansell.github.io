bac97a4471b57f973604c0aef89813d3e80f557d
Joe Ansell <jaj0eansell@gmail.com>
2026-08-07 21:53:33 +0100

    2026 Update, four new pages and homepage redesign

--- script.js
+++ script.js
@@ -447,6 +447,213 @@ function overlayOn18() {
   //Myrick, D. Sanchez, E. (Directors). (1999). The Blair Witch Project. Haxan Films.
 }
 
+function overlayOn19() {
+	//Makes the overlay appear on screen
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/zombie.PNG";
+    document.getElementById("overlayImg").style.width = "80%";
+    document.getElementById("overlayImg").style.height = "35%";
+    document.getElementById("overlayImg").style.left = "10%";
+    document.getElementById("overlayImg").style.right = "10%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A still from the game Final Fantasy Legend, visually edited.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/zombie.PNG";
+    document.getElementById("overlayImg").style.width = "50%";
+    document.getElementById("overlayImg").style.height = "80%";
+    document.getElementById("overlayImg").style.left = "25%";
+    document.getElementById("overlayImg").style.right = "25%";
+    document.getElementById("overlayImg").style.top = "15%";
+    document.getElementById("overlayText").innerHTML = "A still from the game Final Fantasy Legend, visually edited.";
+  }
+}
+
+function overlayOn20() {
+	//Makes the overlay appear on screen
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/skeleton.PNG";
+    document.getElementById("overlayImg").style.width = "80%";
+    document.getElementById("overlayImg").style.height = "35%";
+    document.getElementById("overlayImg").style.left = "10%";
+    document.getElementById("overlayImg").style.right = "10%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A still from the game Final Fantasy Legend, visually edited.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/skeleton.PNG";
+    document.getElementById("overlayImg").style.width = "50%";
+    document.getElementById("overlayImg").style.height = "80%";
+    document.getElementById("overlayImg").style.left = "25%";
+    document.getElementById("overlayImg").style.right = "25%";
+    document.getElementById("overlayImg").style.top = "15%";
+    document.getElementById("overlayText").innerHTML = "A still from the game Final Fantasy Legend, visually edited.";
+  }
+}
+
+function overlayOn21() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/imageedit.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "25%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A screenshot of sprites being changed in a tile editor.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/imageedit.PNG";
+    document.getElementById("overlayImg").style.width = "60%";
+    document.getElementById("overlayImg").style.height = "70%";
+    document.getElementById("overlayImg").style.left = "20%";
+    document.getElementById("overlayImg").style.right = "20%";
+    document.getElementById("overlayImg").style.top = "18%";
+    document.getElementById("overlayText").innerHTML = "A screenshot of sprites being changed in a tile editor.";
+  }
+}
+
+function overlayOn22() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/clim8.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "25%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A slide comprising part of a feature review of a competing device.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/clim8.PNG";
+    document.getElementById("overlayImg").style.width = "60%";
+    document.getElementById("overlayImg").style.height = "70%";
+    document.getElementById("overlayImg").style.left = "20%";
+    document.getElementById("overlayImg").style.right = "20%";
+    document.getElementById("overlayImg").style.top = "18%";
+    document.getElementById("overlayText").innerHTML = "A slide comprising part of a feature review of a competing device.";
+  }
+}
+
+function overlayOn23() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/hardware.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "25%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A slide showcasing different devices and materials for use in the creation of an IoT Device.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/hardware.PNG";
+    document.getElementById("overlayImg").style.width = "60%";
+    document.getElementById("overlayImg").style.height = "70%";
+    document.getElementById("overlayImg").style.left = "20%";
+    document.getElementById("overlayImg").style.right = "20%";
+    document.getElementById("overlayImg").style.top = "18%";
+    document.getElementById("overlayText").innerHTML = "A slide showcasing different devices and materials for use in the creation of an IoT Device.";
+  }
+}
+
+function overlayOn24() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/productdesign.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "25%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "A slide showcasing the design concept for the Heat smart jacket, with practical justification.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/productdesign.PNG";
+    document.getElementById("overlayImg").style.width = "60%";
+    document.getElementById("overlayImg").style.height = "70%";
+    document.getElementById("overlayImg").style.left = "20%";
+    document.getElementById("overlayImg").style.right = "20%";
+    document.getElementById("overlayImg").style.top = "18%";
+    document.getElementById("overlayText").innerHTML = "A slide showcasing the design concept for the Heat smart jacket, with practical justification.";
+  }
+}
+
+function overlayOn25() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/abstract.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "25%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "The abstract of my dissertation, providing an overview of it's contents.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/abstract.PNG";
+    document.getElementById("overlayImg").style.width = "50%";
+    document.getElementById("overlayImg").style.height = "55%";
+    document.getElementById("overlayImg").style.left = "25%";
+    document.getElementById("overlayImg").style.right = "25%";
+    document.getElementById("overlayImg").style.top = "24%";
+    document.getElementById("overlayText").innerHTML = "The abstract of my dissertation, providing an overview of it's contents.";
+  }
+}
+
+function overlayOn26() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/graphs.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "28%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "Bar charts summarising some of the results of my study, regarding numbers of artificial posts on social media.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/graphs.PNG";
+    document.getElementById("overlayImg").style.width = "45%";
+    document.getElementById("overlayImg").style.height = "52%";
+    document.getElementById("overlayImg").style.left = "27%";
+    document.getElementById("overlayImg").style.right = "27%";
+    document.getElementById("overlayImg").style.top = "24%";
+    document.getElementById("overlayText").innerHTML = "Bar charts summarising some of the results of my study, regarding numbers of artificial posts on social media.";
+  }
+}
+
+function overlayOn27() {
+	//If controls mobile view, else controls desktop
+  if (x.matches) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/summary.PNG";
+    document.getElementById("overlayImg").style.width = "90%";
+    document.getElementById("overlayImg").style.height = "28%";
+    document.getElementById("overlayImg").style.left = "5%";
+    document.getElementById("overlayImg").style.right = "5%";
+    document.getElementById("overlayImg").style.top = "30%";
+    document.getElementById("overlayText").innerHTML = "An Excel table, showcasing the totals of my analysis of AI posts on social media.";
+  }else{
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlayImg").src = "images/summary.PNG";
+    document.getElementById("overlayImg").style.width = "40%";
+    document.getElementById("overlayImg").style.height = "50%";
+    document.getElementById("overlayImg").style.left = "30%";
+    document.getElementById("overlayImg").style.right = "30%";
+    document.getElementById("overlayImg").style.top = "24%";
+    document.getElementById("overlayText").innerHTML = "An Excel table, showcasing the totals of my analysis of AI posts on social media.";
+  }
+}
+
 function overlayOff() {
 	//Sets the overlay to no longer display
   document.getElementById("overlay").style.display = "none";
